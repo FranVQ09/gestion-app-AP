05bf189a1dc23ed97557ac12b43bd16c1dc11eaf
josejulian1604 <85717661+josejulian1604@users.noreply.github.com>
2024-03-29 13:09:37 -0600

    Se crea la aplicacion para celular con libreria react

---
+++ my-app/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <>
+      <View style={styles.container}></View>
+      <WebView source={{ uri: 'https://bucolic-ganache-4cf7a5.netlify.app/' }} style={{ flex: 1 }} />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 45,
+  },
+});
