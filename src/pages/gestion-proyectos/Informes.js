680c0c8b24ae204b53ca7f0b26b82af12904c11e
FranVQ09 <franciscovq@estudiantec.cr>
2024-05-28 13:02:20 -0600

    CSS Informes

--- src/pages/gestion-proyectos/Informes.js
+++ src/pages/gestion-proyectos/Informes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import '../../styles/ModificarProyecto.css';
+import '../../styles/Informes.css';
 
 function Informes() {
   return (
