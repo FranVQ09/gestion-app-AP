325ae7777eb4906590d82dbdaba625550b812e5d
FranVQ09 <franciscovq@estudiantec.cr>
2024-05-28 13:06:56 -0600

    Avances

--- src/pages/gestion-proyectos/Informes.js
+++ src/pages/gestion-proyectos/Informes.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import db from '../../fisebaseConfig/firebaseConfig';
+import { collection, getDocs, doc, updateDoc, addDoc, query, where } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 import '../../styles/Informes.css';
 
 function Informes() {
+    const [proyectos, setProyectos] = useState([]);
+    const [proyectoSeleccionado, setProyectoSeleccionado] = useState('');
+
+    useEffect(() => {
+        const obtenerProyectos = async () => {
+            const proyectosCollection = collection(db, 'proyecto');
+            const querySnapshot = await getDocs(proyectosCollection);
+            const proyectosData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setProyectos(proyectosData);
+        };
+
+        obtenerProyectos();
+    }, []);
+
   return (
     <div className='content'>
         <div className='flex-div'>
@@ -11,8 +27,11 @@ function Informes() {
             </div>
             <form className='mproye'>
                 <label htmlFor="proyecto">Seleccione un Proyecto:</label>
-                <select className='laselecta'>
-
+                <select className='laselecta' id="proyecto" value={proyectoSeleccionado} onChange={(e) => setProyectoSeleccionado(e.target.value)}>
+                <option value="">Seleccione un proyecto</option>
+                    {proyectos.map(proyecto => (
+                        <option key={proyecto.id} value={proyecto.nombreProyecto}>{proyecto.nombreProyecto}</option>
+                    ))}
                 </select>
                 <Link className='back' to="/gestionProyectos">Regresar</Link>
             </form>
